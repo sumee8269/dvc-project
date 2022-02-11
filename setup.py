55bde259f044d199b0f9cc29545aeb40264252a9
sumee8269 <sumit.thakur12492@gmail.com>
2022-02-11 21:01:18 +0530

    stageg02

--- setup.py
+++ setup.py
@@ -1,22 +1,23 @@
-    from setuptools import setup,find_packages
+from setuptools import setup,find_packages
 
-    with open("README.md","r", encoding="utf-8") as f:
-        long_description = f.read()
+with open("README.md","r", encoding="utf-8") as f:
+    long_description = f.read()
 
-    setup(
-        name="src",
-        version="0.0.1",
-        author="sumee8269",
-        description = "A small package for dcv ml pipeline demo",
-        long_description= long_description,
-        url = "https://github.com/sumee8269/simple-dvc-project.git",
-        long_description_content_type = "text/markdown",
-        author_email="sumit.thakur12492@gmail.com",
-        package_dir={"":"src"},
-        packages=find_packages(where="src"),
-        license="GNU",
-        python_requires = ">=3.6",
-        install_requires = ['dvc','dvc[gdrive]','dvc[s3]','pandas',
+setup(
+    name="src",
+    version="0.0.1",
+    author="sumee8269",
+    description = "A small package for dcv ml pipeline demo",
+    long_description= long_description,
+    url = "https://github.com/sumee8269/simple-dvc-project.git",
+    long_description_content_type = "text/markdown",
+    author_email="sumit.thakur12492@gmail.com",
+    #package_dir={"":"src"},
+    #packages=find_packages(where="src"),
+    packages=["src"],
+    license="GNU",
+    python_requires = ">=3.6",
+    install_requires = ['dvc','dvc[gdrive]','dvc[s3]','pandas',
                             'scikit_learn']
 
-    )
+)
